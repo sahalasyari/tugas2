547a899e1ccc316812687fba749ec455a9623b55
sahalasyari <114453017+sahalasyari@users.noreply.github.com>
2023-10-05 13:40:32 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+// variabel firstName dengan tipe data string
+var firstName = "Moh sahal";
+
+// variabel lastName dengan tipe data string
+var lastName = "Assahari";
+
+// variabel age dengan tipe data number
+var age = 20;
+
+// isMarried dengan tipe data boolean
+var isMarried = false;
+
+
+console.log("Nama Depan:", firstName);
+console.log("Nama Belakang:", lastName);
+console.log("Umur:", age);
+console.log("Status Pernikahan:", isMarried);
